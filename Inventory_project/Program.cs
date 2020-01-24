1fbab3cfd20bd8cea9daeb24c4de4e75623ce8dd
Turcsanyadam <turcsanyadam@gmail.com>
2020-01-24 19:10:02 +0100

    Add project files.

---
+++ Inventory_project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inventory_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StoreManager storeManager = new StoreManager();
+        }
+    }
+}
---
+++ Inventory_project/StoreManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory_project
+{
+    class StoreManager : StorageCapable
+    {
+        StorageCapable storageCapable;
+        public StoreManager(StorageCapable storageCapable)
+        {
+            this.storageCapable = storageCapable;
+        }
+    }
+}
